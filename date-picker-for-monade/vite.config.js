fa6d65ecf1f5e696e17e0b30a85b7b5f3bed3af2
Alexandre <alexandremontagna@gmail.com>
2024-04-10 08:05:42 +0200

    config: Change development server port to 3000 in vite.config.js

--- date-picker-for-monade/vite.config.js
+++ date-picker-for-monade/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Set the port to 3000
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
